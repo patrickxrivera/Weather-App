e086b67c9b40b7ca1f9cd0f87c8a82e7a74e5ae2
Patrick <patrick.x.rivera@gmail.com>
2018-01-14 21:35:58 -0500

    Added outline

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // use resources from here to get openweather to work in CodePen
 // use navigator.geolocation method for geolocation
+
+/*
+  1) Get weather data from current location
+    // get current location
+    // make API call for current weather data
+    // Today - a) current weather, day of the week, day of the month, wind, weather type, city, state, time
+    // Forecast day of the week, weather type, average weather
+  2) Update styling based on weather type
+    // large current Weather
+    // small current weather
+    // 5-day forecast
+    // top-bg color, top font color
+  3) Animations
+  4) toggle celsius and fahrenheit
+*/
+// get current
